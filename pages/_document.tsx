b33825b56edff3fe9c99e260caa60015c104b1a9
erlanig <erlanig@students.undip.ac.id>
2025-06-08 21:27:07 +0700

    update meta name

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,9 +5,9 @@ const Document = () => {
   return (
     <Html lang='en'>
       <Head>
-          <meta name="description" content="An company profile of Collosal Company. Its company that serve for Application Development (Mobile, Web and Desktop), UI UX Design and Maintenance" />
+          <meta name="description" content="Joki Coding | Joki Pemrograman | Joki Tugas | Tugas Umum. 💻 Expert in Coding & IT Solutions. 🚀 Custom Projects | General Tasks Made Easy. 📄 Real-time Quiz Assistance. 📞 +62 851‑7442‑4245" />
           <meta name="robots" content="index, follow" />
-          <meta name='tags' content='web,app,ui,maintenance,company,collosal,profile' />
+          <meta name="tags" content="joki coding, joki pemrograman, joki tugas, tugas umum, coding, IT solutions, custom projects, quiz assistance, expert, jasa pemrograman" />
         <link
           rel="shortcut icon"
           href="/icon.png"
